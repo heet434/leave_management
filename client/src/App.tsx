68da54fbf63f15256f36888fa51492dfa8988aae
Heet Patel <hiheet04@gmail.com>
2024-06-20 18:23:37 +0530

    updates

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -52,7 +52,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
+      {/* <Router basename='/client'> basename='/client' is used to make sure the routes are correct when deployed to the server. */}
         <Routes>
           <Route path="/" element = {<SignIn userLoginUtility={userLoginUtility}/>}/>
           <Route path="/signup" element = {<SignUp/>}/>
@@ -60,7 +60,7 @@ function App() {
           <Route path="/instructor" element = {<Instructor userLoginUtility={userLoginUtility}/>}/>
           <Route path="/admin" element = {<Admin userLoginUtility={userLoginUtility}/>}/>
         </Routes>
-      </Router>
+      {/* </Router> */}
     </div>
   );
 }
